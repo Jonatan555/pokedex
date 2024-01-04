f2d70b00a11e484c3d2bb9c03a47c7686a08bc56
Jonatan melo <jonatanmelo058@gmail.com>
2024-01-03 21:18:52 -0300

    add base styles components

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+
+export function App() {
+  return (
+    <Container>
+      <header>Header</header>
+      
+      <main>
+      <h1> App </h1>
+      </main>
+      <footer>footer</footer>
+    </Container>
+  );
+}
